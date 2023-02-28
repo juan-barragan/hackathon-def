e60499748815ef95d5e59cdd3f7b0a5958db50ca
drommk <eric.palle.tech@gmail.com>
2023-02-28 15:51:46 +0100

    add rect
    add rect paris

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import pandas as pd
-import streamlit as st
 import numpy as np
+
+paris_depts = [91,92,75,77,93,94,95,78]
 
 def load_accidents(file_name):
     data = pd.read_csv(file_name, dtype = {'lon': float, 'lat':float})
     data = data.dropna(subset=['lon', 'lat'])
-    data = data[ (data['lon'] != 0) & (data['lat'] != 0) ]
+    data = data[data['departement'].isin([str(dept) for dept in paris_depts])]
+    data = data[data['lon'].between(2.1, 2.5)]
+    data = data[data['lat'].between(48.75, 48.95)]
+
     return data
 
 st.title("Velo hacks")
@@ -15,5 +19,7 @@ file_name = 'accidents-velos.csv'
 
 accidents = load_accidents(file_name)
 st.map(accidents)
+
+
 
 
