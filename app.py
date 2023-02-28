66fc8e7d1a115441404e71f2d146448a9585a806
drommk <eric.palle.tech@gmail.com>
2023-02-28 16:38:03 +0100

    render all in streamlit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import seaborn as sns
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 paris_depts = [91,92,75,77,93,94,95,78]
 
@@ -13,13 +15,44 @@ def load_accidents(file_name):
 
     return data
 
+def get_age(row):
+    x, y = map(int, row.split('-'))
+    return (x+y)/2
+
 st.title("Velo hacks")
 
 file_name = 'accidents-velos.csv'
 
-accidents = load_accidents(file_name)
-st.map(accidents)
+data_set = load_accidents(file_name)
+st.map(data_set)
+
+data_set['mean_age'] = data_set['age'].apply(get_age)
+data_set = data_set[ data_set['mean_age'] < 100]
+fig, ax = plt.subplots()
+sns.histplot(data=data_set, x="mean_age", binwidth=1, ax=ax)
+
+st.pyplot(fig)
+
+fig2, ax = plt.subplots()
+sns.countplot(x=data_set["sexe"])
+st.pyplot(fig2)
+
+fig3, ax = plt.subplots()
+g = sns.countplot(x=data_set["gravite accident"])
+g.set_xticklabels(g.get_xticklabels(), rotation=90)
+st.pyplot(fig3)
+
+fig4, ax = plt.subplots()
+g = sns.countplot(x = data_set['motif deplacement'])
+g.set_xticklabels(g.get_xticklabels(), rotation=90)
+st.pyplot(fig4)
+
+fig6, ax = plt.subplots()
+sns.countplot(data=data_set, y="existence securite", hue="gravite accident")
+st.pyplot(fig6)
 
 
-
-
+fig7, ax = plt.subplots()
+g = sns.countplot(x = data_set[ data_set['gravite accident'] == '3 - Tué' ]['existence securite'])
+g.set_xticklabels(g.get_xticklabels(), rotation=90)
+st.pyplot(fig7)
